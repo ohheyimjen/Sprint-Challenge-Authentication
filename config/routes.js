867e8f5296fe08b63e2c026e8d20039acbb1e383
ohheyimjen <jendority@gmail.com>
2019-05-17 08:55:46 -0700

    added register/login functions. created users-model.js

--- config/routes.js
+++ config/routes.js
@@ -2,6 +2,9 @@ const axios = require('axios');
 
 const { authenticate } = require('../auth/authenticate');
 
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 module.exports = server => {
   server.post('/api/register', register);
   server.post('/api/login', login);
@@ -10,11 +13,39 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function login(req, res) {
   // implement user login
-}
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if(user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Well hello ${user.username}! Take a token!`, token,
+        });
+      } else {
+        res.status(401).json({ message: 'Sorry, Charlie. You need a token to get in' })
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+};
 
 function getJokes(req, res) {
   const requestOptions = {
