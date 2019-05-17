f0e08ffe862bcd28edf03d23fa54fa15426fe087
ohheyimjen <jendority@gmail.com>
2019-05-17 09:57:12 -0700

    completed MVPs

--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,12 @@
 const axios = require('axios');
+const router = require('express').Router();
 
-const { authenticate } = require('../auth/authenticate');
+const { authenticate, jwtKey } = require('../auth/authenticate');
 
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Users = require('../users/users-model');
+
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -29,12 +32,14 @@ function register(req, res) {
 function login(req, res) {
   // implement user login
   let { username, password } = req.body;
-
+console.log(1);
   Users.findBy({ username })
     .first()
-    .then(user => {
-      if(user && bcrypt.compareSync(password, user.password)) {
+    .then(user => { 
+      console.log(2);
+      if(user && bcrypt.compareSync(password, user.password)) { console.log(3);
         const token = generateToken(user);
+        console.log(4);
         res.status(200).json({
           message: `Well hello ${user.username}! Take a token!`, token,
         });
@@ -61,3 +66,17 @@ function getJokes(req, res) {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
 }
+
+// function for generating token
+function generateToken(user) { console.log(user);
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  }
+  const options = {
+    expiresIn: '1d',
+  }
+  return jwt.sign(payload, jwtKey, options);
+}
+
+// module.exports = router;
